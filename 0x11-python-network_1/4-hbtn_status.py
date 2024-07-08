0ea2d0b2a3b736c9aebcc32152f30d3b98b6ecb9
jdjskah <bannouryayoub@hotmail.com>
2024-07-07 19:32:37 -0700

    bruv

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -1,10 +1,12 @@
 #!/usr/bin/python3
-"""fetches https://intranet.hbtn.io/status."""
-import requests
-
-
+"""script for testing status of web pages
+"""
 if __name__ == "__main__":
-    r = requests.get("https://intranet.hbtn.io/status")
-    print("Body response:")
-    print("\t- type: {}".format(type(r.text)))
-    print("\t- content: {}".format(r.text))
+    import requests
+    url = "https://intranet.hbtn.io/status"
+    response = requests.get(url)
+    content = response.text
+    print_str = '''Body response:
+\t- type: {}
+\t- content: {}'''.format(type(content), content)
+    print(print_str)
